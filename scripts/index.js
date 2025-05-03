3eb8b9ba2b332d8b8ab34dd77412eed1a9b67422
cdubb919 <christinanw919@gmail.com>
2025-05-03 03:34:57 -0500

    project 5 phase 2

--- scripts/index.js
+++ scripts/index.js
@@ -45,37 +45,44 @@ const profileDescriptionEl = document.querySelector(".profile__description");
 const newPostImageLink = document.querySelector(".card__image");
 const newPostCaption = document.querySelector(".card__title");
 
+function openModal(modal) {
+    modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+    modal.classList.remove("modal_is-opened");
+}
 
 editProfileBtn.addEventListener("click", function () {
     editProfileNameInput.value = profileNameEl.textContent;
     editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-    editProfileModal.classList.add("modal_is-opened");
+    openModal(editProfileModal);
 })
 
 editProfileCloseBtn.addEventListener("click", function () {
-    editProfileModal.classList.remove("modal_is-opened");
+    closeModal(editProfileModal);
 })
 
 newPostBtn.addEventListener("click", function () {
     newPostCardImageInput.value = newPostImageLink.textContent;
     newPostCaptionDescription.value = newPostCaption.textContent;
-    newPostModal.classList.add("modal_is-opened");
+    openModal(newPostModal);
 })
 
 newPostCloseBtn.addEventListener("click", function () {
-    newPostModal.classList.remove("modal_is-opened");
+    closeModal(newPostModal);
 })
 
 function handleEditProfileSubmit(evt) {
     evt.preventDefault();
     profileNameEl.textContent = editProfileNameInput.value;
     profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-    editProfileModal.classList.remove("modal_is-opened");
+    closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
-initialCards.forEach(function (item) {
+intialCards.forEach(function (item) {
     console.log(item.name);
     console.log(item.link);
 });
